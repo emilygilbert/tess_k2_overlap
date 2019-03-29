b376e6fb32a60f484befa309c306d0d7fe3690b4
emilygilbert <emilygilbert22@gmail.com>
2019-03-29 12:57:01 -0500

    added missing parameter file

--- parameters.py
+++ parameters.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
 """
 Thanks to Christina Hedges for the NEXSCI data retrieval function
 """
@@ -17,7 +23,7 @@ def retrieve_online_data():
                 'pl_tranflag,pl_trandur,pl_pnum,pl_k2flag,pl_kepflag,pl_facility,'
                 'pl_orbincl,pl_orbinclerr1,pl_orbinclerr2,pl_orblper,st_mass,st_masserr1,'
                 'st_masserr2,st_rad,st_raderr1,st_raderr2,st_teff,st_tefferr1,'
-                'st_tefferr2,st_optmag,st_j,st_h', comment='#')
+                'st_tefferr2,st_optmag,st_j,st_h,pl_imppar,pl_impparerr1,pl_impparerr2', comment='#')
         composite = pd.read_csv(NEXSCI_API + '?table=compositepars&select=fpl_hostname,'
                     'fpl_letter,fpl_smax,fpl_smaxerr1,fpl_smaxerr2,fpl_radj,fpl_radjerr1,'
                     'fpl_radjerr2,fpl_bmassj,fpl_bmassjerr1,fpl_bmassjerr2,fpl_eqt,'
@@ -34,16 +40,68 @@ def retrieve_online_data():
          right_on=['pl_hostname', 'pl_letter'])
     return df[df.pl_tranflag == 1]
 
-def save_nexsci_output():
+
+# In[ ]:
+
+
+def save_nexsci_output():    
     df = retrieve_online_data()
     df.to_csv('nexsci_output.csv')
 
 def get_number_of_planets(hostname):
     n = np.asarray(df.loc[df.pl_hostname == hostname][['pl_pnum']])[0]
-
+    
     return n
 
 def get_planet_params(hostname, planet_letter='b'):
-    period, t0, dur, depth = np.asarray(df.loc[(df.pl_hostname == hostname) & (df.pl_letter == planet_letter)][['pl_orbper', 'pl_tranmid', 'pl_trandur', 'pl_trandep']])[0]
+    df = retrieve_online_data()
+    period, t0, dur, depth, impact_param, r_planet, r_star = np.asarray(df.loc[(df.pl_hostname == hostname) & 
+                                               (df.pl_letter == planet_letter)][['pl_orbper', 'pl_tranmid',
+                                                'pl_trandur', 'pl_trandep', 'pl_imppar', 'pl_radj', 'st_rad']])[0]
+    
+    return period, t0, dur, depth, impact_param, r_planet, r_star
 
-    return period, t0, dur, depth
+
+# In[ ]:
+
+
+def define_offsets():
+    tess_offset = 2457000
+    k2_offset = 2454833
+    return tess_offset, k2_offset
+
+def get_params(hostname):
+    period, t0, dur, depth, impact_param, r_planet, r_star = get_planet_params(hostname)
+    return period, t0, dur, depth, impact_param, r_planet, r_star
+
+
+def generate_inputs(period, t0, dur, depth, impact_param, r_planet, r_star):
+    r, r_min, r_max = generate_radius_ratio(depth, r_planet, r_star)
+    b_test = generate_impact_parameter(impact_param)
+    
+    return period, t0, dur, depth, impact_param, r_planet, r_star
+
+def generate_radius_ratio(depth, r_planet, r_star ):
+    if pd.notna(depth):
+        r_ratio = np.sqrt(depth)
+        min_radius = r_ratio - .5*r_ratio
+        max_radius = r_ratio + .5*r_ratio
+    elif (pd.notna(r_planet)) & (pd.notna(r_star)):
+        r_ratio = (r_planet*71492) / (r_star*695700)
+        min_radius = r_ratio - .5*r_ratio
+        max_radius = r_ratio + .5*r_ratio
+    else:
+        r_ratio = .01
+        min_radius=0.001
+        max_radius=0.1
+        
+    return r_ratio, min_radius, max_radius
+
+def generate_impact_parameter(impact_param):
+    if pd.notna(impact_param):
+        b = impact_param
+    else:
+        b = 0.5
+    
+    return b
+
